0ea8561b89aa4de2124aaa46c1e94f37fecc853b
Katsumi Sumiya <kachan.0627.619@gmail.com>
2021-09-20 02:51:36 +0900

    テスト修正

--- src/getTimezoneOffsetHours.test.js
+++ src/getTimezoneOffsetHours.test.js
@@ -1,5 +1,4 @@
-const { TestScheduler } = require('@jest/core');
 const getTimezoneOffsetHours = require('./getTimezoneOffsetHours');
-TestScheduler('Success return -9', () => {
+test('Success return -9', () => {
   expect(getTimezoneOffsetHours()).toBe(-9);
 });
